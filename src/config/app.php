6c55dc65f76edff9e752cf343857f1baaf683d73
blawidris <blawidris@gmail.com>
2023-11-13 02:01:09 -0500

    added db manager line to composer

--- src/config/app.php
+++ src/config/app.php
@@ -177,7 +177,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        PrevailExcel\Nowpayments\NowpaymentsServiceProvider::class,
+        // PrevailExcel\Nowpayments\NowpaymentsServiceProvider::class,
 
     ],
 
@@ -234,7 +234,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'QrCode' => SimpleSoftwareIO\QrCode\Facades\QrCode::class,
-        'Nowpayments' => PrevailExcel\Nowpayments\Facades\Nowpayments::class,
+        // 'Nowpayments' => PrevailExcel\Nowpayments\Facades\Nowpayments::class,
     ],
 
 ];
